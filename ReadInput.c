617f26448412e46d9367f0d12c167a1948cb0670
Circle <circles7en@inferno.(none)>
2013-02-12 16:48:42 -0600

    Adding an example of scanf

---
+++ ReadInput.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+   
+   int number;
+
+   printf("Please type a single number and hit Enter: ");
+   scanf("%i", &number);
+   printf("You typed %i. \n", number);
+
+}
